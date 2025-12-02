2b01b43a03e02f5629e55d1526245fabf5b9cd1f
Bassel T <lessab99@gmail.com>
2025-12-01 23:18:12 -0600

    Completed Day 2 of 2025

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day1.Part1();
-            Day1.Part2();
+            Day2.Part1();
+            Day2.Part2();
         }
     }
 }
---
+++ Year2025/Day2.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2025
+{
+    public static class Day2
+    {
+        public static void Part1()
+        {
+            using (var reader = new StreamReader("input.txt"))
+            {
+                var ranges = reader.ReadLine().Split(',');
+                long invalid = 0;
+
+                foreach (var range in ranges)
+                {
+                    var limits = range.Split('-').Select(long.Parse).ToArray();
+
+                    for (long i = limits[0]; i <= limits[1]; i++)
+                    {
+                        var str = i.ToString();
+                        var len = str.Length;
+                        if (str.Substring(0, len / 2) == str.Substring(len / 2))
+                        {
+                            Console.WriteLine(str);
+                            invalid += i;
+                        }
+                    }
+                }
+
+                Console.WriteLine(invalid);
+
+            }
+        }
+
+        public static void Part2()
+        {
+            using (var reader = new StreamReader("input.txt"))
+            {
+                var ranges = reader.ReadLine().Split(',');
+                long invalid = 0;
+
+                foreach (var range in ranges)
+                {
+                    var limits = range.Split('-').Select(long.Parse).ToArray();
+
+                    for (long i = limits[0]; i <= limits[1]; i++)
+                    {
+                        var str = i.ToString();
+
+                        for (int len = 1; len < str.Length; len++)
+                        {
+                            var substring = str.Substring(0, len);
+                            if (str.Replace(substring, "").Length == 0)
+                            {
+                                Console.WriteLine(str);
+                                invalid += i;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine(invalid);
+
+            }
+        }
+    }
+}
